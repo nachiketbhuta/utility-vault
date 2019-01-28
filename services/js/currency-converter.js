1b3e52e9cf164455b32b8f50c104ccb78907017a
himanshu <himanshu.tigaiya@gmail.com>
2019-01-28 19:45:09 +0530

    currency converter UI & JS changes

--- services/js/currency-converter.js
+++ services/js/currency-converter.js
@@ -1,34 +1,78 @@
-if ($) {
-  $("#submit").click(function(e) {
-    e.preventDefault();
+/*
+  API FROM https://www.exchangerate-api.com
+*/
 
-    var from_type = $("#from")
-      .val()
-      .toUpperCase();
-    var from_amt = $("#from_amt").val();
-    var to_type = $("#to")
-      .val()
-      .toUpperCase();
+$(() => {
+    const API_KEY = "362a95f4b044739b89c01431";
+    const URL = `https://v3.exchangerate-api.com/bulk/${API_KEY}/INR`;
 
-    const API_KEY = "g941JYvtybNOm9VPylWFzbwtOMNXDsTl";
-
-    const URL = `https://forex.1forge.com/1.0.3/convert?from=${from_type}&to=${to_type}
-                &quantity=${from_amt}&api_key=${API_KEY}`;
+    $.get('../services/misc/curr_data.json',function(data){
+        curr_data = data;
+    },'json');
 
     $.get({
-      url: URL,
-      cors: true,
-      secure: true,
-      success: function(data) {
-        console.log(data);
-        $("#to_amt").val(data.value);
-        $("#to_amt").attr("disabled", "disabled");
-      },
-      error: function(error) {
-        console.log(error);
-      }
+        url: URL,
+        cors: true,
+        secure: true,
+        success: function (data) {
+            if (data.result == "success") {
+                $.each(data.rates, function (key, value) {
+                    $('#from').append($("<option></option>")
+                        .attr("value", key)
+                        .text(key));
+                    $('#to').append($("<option></option>")
+                        .attr("value", key)
+                        .text(key));
+                });
+            }
+        },
+        error: function (error) {
+            console.log(error);
+        }
     });
-  });
-} else {
-  console.log("No Jquery");
-}
+
+    $('select').change(function(){
+        console.log(this);
+        var id = $(this).prop("id");
+        var select_val = $.trim($(this).val()).toUpperCase();
+        var curr_full = "";
+        $.each(curr_data, function(index, value){
+            if(value['curr_short'] == select_val){
+                curr_full = value['curr_full'];
+            }
+        });
+        if(id == "from"){
+            $('#from_curr_help').css("display", "block");
+            $('#from_curr_help').text(curr_full);
+        }else if(id == "to"){
+            $('#to_curr_help').css("display", "block");
+            $('#to_curr_help').text(curr_full);
+        }
+    });
+
+    $("#submit").click(function (e) {
+        e.preventDefault();
+
+        var from_type = $("#from")
+            .val()
+            .toUpperCase();
+        var from_amt = $("#from_amt").val();
+        var to_type = $("#to")
+            .val()
+            .toUpperCase();
+
+        $.get({
+            url: URL,
+            cors: true,
+            secure: true,
+            success: function (data) {
+                if (data.result == "success") {
+                    $("#to_amt").val(data.rates[to_type] * from_amt);
+                }
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    });
+});
