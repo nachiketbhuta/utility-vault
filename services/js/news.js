2876b8cc1754ad5fdc02f062b9947385464afebf
nachiketbhuta <nachiketbhuta@gmail.com>
2019-04-25 20:16:04 +0530

    news cards are added

--- services/js/news.js
+++ services/js/news.js
@@ -12,9 +12,7 @@ $(() => {
             url: URL,
             cors: true,
             success: function (data) {
-
                 let articles = data.articles;
-
                 console.log(articles);
 
                 $('.news').remove();
@@ -22,17 +20,21 @@ $(() => {
                 if (articles.length > 0) {
                     $.each(articles, function(index, value) {
                         $(".news-container").append(`
-                            <div class="news">
-			                    <div class="news-image">
-				                    <img src=${value['urlToImage']} width="150" height="150"  alt="">	
-			                    </div>
-			                    <div class="news-content">
-				                    <h3>${value['title']}</h3>
-				                    <p>${value['description']}</p>
-                                    <span>Author: ${value['author']}</span><br>
-                                    <a class="news-link" href=${value['url']}>Link to this article</a>
-			                    </div>
-		                    </div>
+                            <div class="card">
+                                <img class="card-img-top" src=${value['urlToImage']} alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">${value['title']}</h5>
+                                    <p class="card-text">${value['description']}</p>
+                                </div>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">Author: ${value['author']}</</li>
+                                    <li class="list-group-item">Source: ${value['source']['name']}</li>
+                                    <li class="list-group-item">Vestibulum at eros</li>
+                                </ul>
+                                <div class="card-body">
+                                    <a href=${value['url']} class="card-link" style="color: var(--dark-color);">Link to the article</a>
+                                </div>
+                            </div>
                         `);
                     });
                 }
