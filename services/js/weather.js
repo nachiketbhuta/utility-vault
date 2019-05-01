4c25709787d16825f6347f07fdb52fd83a1f2e7b
nachiketbhuta <nachiketbhuta@gmail.com>
2019-05-01 23:12:07 +0530

    Changes in weather page

--- services/js/weather.js
+++ services/js/weather.js
@@ -2,10 +2,8 @@
 	http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=c3584b224afd6dcc0b75b8e0d69f4a93&units=metric
 	http://openweathermap.org/img/w/${icon}.png
 */
-
 $(() => {
-
-	toastr.options = {
+    toastr.options = {
         "closeButton": true,
         "debug": false,
         "newestOnTop": false,
@@ -21,45 +19,37 @@ $(() => {
         "hideEasing": "linear",
         "showMethod": "fadeIn",
         "hideMethod": "fadeOut"
-      }
-
-	
-	$(".submit").on("click", (e) => {
-		e.preventDefault();
-		$.busyLoadFull('show');
-
-		let city = $.trim($(".search").val());
-
-		const URL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=c3584b224afd6dcc0b75b8e0d69f4a93&units=metric`;
-
-		$.get({
-			url: URL,
-			cors: true,
-			dataType: "jsonp",
-			success: function(data) {
-				console.log(data);
-				
-				if (data.cod === 200) {
-					$(".weather").css('display', 'initial');
-	
-					$(".temp").html(Math.floor(data.main.temp) + "&deg;");
-					$(".location").text(data.name);
-					$(".rain").text(data.clouds.all + "\nMM");
-					$(".wind").text(data.wind.speed + "\nKMPH");
-
-					$('#weatherIcon').attr('src', `http://openweathermap.org/img/w/${data.weather[0].icon}.png`)
-				} 
-				else {
-					toastr['error']("No city related to your query was found");
-				}
-
-				$.busyLoadFull('hide');
-			},
-			error: function(error) {
-				console.log(error);
-				$.busyLoadFull('hide');
+    }
+    $(".submit").on("click", (e) => {
+        e.preventDefault();
+        $.busyLoadFull('show');
+        let city = $.trim($(".search").val());
+        const URL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=c3584b224afd6dcc0b75b8e0d69f4a93&units=metric`;
+        $.get({
+            url: URL,
+            cors: true,
+            dataType: "jsonp",
+            success: function(data) {
+                console.log(data);
+                if (data.cod === 200) {
+                    $(".weather").css('display', 'initial');
+                    $(".temp").html(Math.floor(data.main.temp) + "&deg;");
+                    $(".location").text(data.name);
+                    $(".rain").text(data.clouds.all + "\nMM");
+                    $(".wind").text(data.wind.speed + "\nKMPH");
+                    $('#weatherIcon').attr('src', `http://openweathermap.org/img/w/${data.weather[0].icon}.png`);
+                    $('.type').text('Weather Type: ' + data.weather[0].main);
+                } else {
+                   toastr['error']("No news related to your query were found");
+                    $('.search').val("");
+                }
+               $.busyLoadFull('hide');
+            },
+            error: function(error) {
+                console.log(error);
+                $.busyLoadFull('hide');
                 toastr.error("Some error occured. Please contact admin");
-			}
-		});
-	});
+            }
+        });
+    });
 });
